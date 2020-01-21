eb9ed3bbd7a6df0e466d63ea23a7e83d72314cb5
Alumnat <Alumnat@HIPERION22.sinf2003srvr2.local>
2020-01-21 19:53:53 +0100

    Datos(Placeholder) + animacion

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    //Esconde los nombres para poder usar fadeIn
+    //Esconde los nombres y datos para poder usar fadeIn y slideDown
     $("span").hide();
+    $(".datos").slideUp(0);
 
     //Animacion fadeIn de los nombres
     $(".circulo").mouseenter(function () {
@@ -12,9 +13,21 @@ $(document).ready(function () {
         $(this).find("span").stop().fadeOut();
     });
 
+    //Animacion slideDown de los datos
+    $(".circulo").click(function () {
 
+        //Si otros datos estan abiertos, los esconde
+        if ($(".datos").is(":visible")) {
+            $(".datos").slideUp();
+        }
 
+        $(this).find(".datos").stop().slideDown();
+        event.stopPropagation();
+    });
 
-
+    //Animacion slideUp de los datos
+    $(document).on("click", function () {
+        $(".datos").slideUp();
+    });
 
 });
